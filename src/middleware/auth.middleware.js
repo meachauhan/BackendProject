fce74a0a51ce04b134f81eb8febe7fb437aa745a
Akash Chauhan <50045501+meachauhan@users.noreply.github.com>
2024-02-22 08:47:14 +0000

    Logout API bug Fixes

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,20 +4,21 @@ import { APIerror } from "../utils/APIerror.js";
 import {User} from "../models/user.models.js"
 export const verifyJWT= asyncHandler (async(req, res, next)=>{
     try {
-        const token=req.cookie.accessToken || req.header("Authorization")?.replace("Bearer ","")
-        if(!token) throw new APIError(401,"Unathorized access ")
-    
-        const decodedToken=await jwt.verify(token,process.env.ACCESS_TOKEN_SECERT)
+        const token= req.header("Authorization")?.replace("Bearer ","")|| req.cookie.accessToken
+        console.log("token", token)
+        if(!token) throw new APIerror(401,"Unathorized access ")
         
+        console.log("Decoded Token: ", jwt.verify(token,process.env.ACCESS_TOKEN_SECRET));
+        const decodedToken=await jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
         const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
-        if(!user) throw new APIError(401,"Invalid access token")
+        if(!user) throw new APIerror(401,"Invalid access token")
         
     
         req.user=user
         next()
     
     } catch (error) {
-        throw new APIError(401,"Invalid access token")
+        throw new APIerror(401,"Invalid access token")
     }
 
 })
