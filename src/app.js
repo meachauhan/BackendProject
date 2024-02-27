10c37838ad7238b729c57703cb3cc3abe500359b
Akash Chauhan <50045501+meachauhan@users.noreply.github.com>
2024-02-27 07:06:48 +0000

    Updated comment router and controller for create and update comment

--- src/app.js
+++ src/app.js
@@ -23,11 +23,12 @@ app.use(cookieParser())
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
-
+import commentRouter from "./routes/comment.routes.js"
 //routes declartion
 app.use('/api/v1/users',userRouter)
 app.use('/api/v1/videos',videoRouter)
 app.use('/api/v1/tweet',tweetRouter)
+app.use('/api/v1/comment',commentRouter)
 // app.user('/docs',swaggerUI.serve,swaggerUI.setup(swaggerfile))
 
 //htpps://localhost:8000/api/v1/users
--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose"
 import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import {APIerror} from "../utils/APIerror.js"
+import {APIResponse} from "../utils/APIResponse.js"
+import asyncHandler from "../utils/asyncHandler.js"
+import {Video} from "../models/video.models.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
@@ -13,10 +14,60 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} =req.params
+    const user= req.user
+    const {content}=req.body
+
+    const video=await Video.findById(videoId)
+    if(!video) throw new APIerror (500, "something went wrong while fetching video details")
+
+    const comment=await Comment.create(
+        {
+            content,
+            video,
+            owner:user
+        }
+    )
+
+    if(!comment) throw new APIerror(500,"Something went wrong while adding comments")
+
+    return res
+    .status(200)
+    .json(
+        new APIResponse(
+            200,
+            comment,
+            "Comment Added Successfully"
+        )
+    )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId}= req.params
+    const {content} =req.body
+
+    const updatedComment=await Comment.findByIdAndUpdate(commentId,
+        {
+            content
+        },
+        {
+            new:true
+        } 
+        )
+
+    if(!updatedComment) throw new APIerror(200,"Something went wrong while updating comment")
+
+    return res
+    .status(200)
+    .json(
+        new APIResponse(
+            200,
+            updatedComment,
+            "Comment updated successfully"
+        )
+    )
+    
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
