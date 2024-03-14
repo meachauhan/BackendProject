4553081c25a93006a0c7964f0e40978e6e4e01c2
Akash Chauhan <50045501+meachauhan@users.noreply.github.com>
2024-03-14 05:59:44 +0000

    API healthCheck endpoint created

--- src/app.js
+++ src/app.js
@@ -1,45 +1,47 @@
-import  express from "express";
+import express from "express";
 
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 // const swaggerUI=require('swagger-ui-express')
 // const swaggerfile=require('../docs/swagger-output.json')
 // import {swd
-const app=express()
+const app = express();
 
 //Basic Middleware configuration
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credential: true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credential: true,
+  }),
+);
 
-app.use(express.json({limit:"16Kb"}))
-app.use(express.urlencoded({limit:"16kb",extended:true}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16Kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 //Swagger
-import swaggerUI from "swagger-ui-express"
-import swaggerDocument from "./utils/swagger-output.json" assert {type:"json"}
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "./utils/swagger-output.json" assert { type: "json" };
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 //routes import
-import userRouter from "./routes/user.routes.js"
-import videoRouter from "./routes/video.routes.js"
-import tweetRouter from "./routes/tweet.routes.js"
-import commentRouter from "./routes/comment.routes.js"
-import likeRouter from "./routes/like.routes.js"
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import healthcheck from "./routes/healthcheck.routes.js";
 //routes declartion
-app.use('/api/v1/users',userRouter)
-app.use('/api/v1/videos',videoRouter)
-app.use('/api/v1/tweet',tweetRouter)
-app.use('/api/v1/comment',commentRouter)
-app.use('/api/v1/like',likeRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/healthcheck", healthcheck);
 // app.user('/docs',swaggerUI.serve,swaggerUI.setup(swaggerfile))
 
 //htpps://localhost:8000/api/v1/users
 
-
-export {app}
+export { app };
--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,11 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import { APIResponse } from "../utils/APIResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
+  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  return res
+    .status(200)
+    .json(new APIResponse(200, { message: "OK" }, "Heathcheck OK"));
+});
 
-export {
-    healthcheck
-    }
-    
+export { healthcheck };
