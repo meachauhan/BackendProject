2438f87c74a64136bb0cbf2498474e1cf4910adf
Akash Chauhan <50045501+meachauhan@users.noreply.github.com>
2024-03-14 11:51:49 +0000

    updated Controller for Getting user playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,6 +1,7 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Playlist } from "../models/playlist.model.js";
 import { Video } from "../models/video.models.js";
+import { User } from "../models/user.models.js";
 // import { ApiError } from "../utils/ApiError.js";
 import { APIResponse } from "../utils/APIResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
@@ -23,6 +24,12 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
   const { userId } = req.params;
   //TODO: get user playlists
+  console.log("Gettting Playlist");
+  const user = await User.findById(userId);
+  const playlists = await Playlist.find({ owner: user._id });
+  return res
+    .status(200)
+    .json(new APIResponse(200, playlists, "Playlists fetched successfully"));
 });
 
 const getPlaylistById = asyncHandler(async (req, res) => {
