56536bde66e455db04f8a092ae087808ece48a7e
Akash Chauhan <50045501+meachauhan@users.noreply.github.com>
2024-03-20 12:37:49 +0000

    updated dashbord to bet Subscriber counts

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,11 +1,35 @@
 import mongoose from "mongoose";
 import { Video } from "../models/video.models.js";
-
+import { Subscription } from "../models/subscriptions.models.js";
 import { APIResponse } from "../utils/APIResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+  const user = req.user;
+  let subscribersCount = await Subscription.aggregate([
+    {
+      $match: {
+        channel: user?._id,
+      },
+    },
+    {
+      $count: "totalSubscriber",
+    },
+  ]);
+  console.log(subscribersCount.hasOwnProperty("totalSubscriber"));
+  if (!subscribersCount.hasOwnProperty("totalSubscriber")) {
+    subscribersCount = {
+      totalSubscriber: 0,
+    };
+  }
+
+  return res
+    .status(200)
+    .json(
+      new APIResponse(200, subscribersCount, "Subscriber Fetched Successfully"),
+    );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
